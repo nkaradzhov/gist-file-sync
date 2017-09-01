f53470fdb82f6f3bde038826f88eedd7978218b4
hukuuu <hukuuu@gmail.com>
2017-09-01 17:54:08 +0300

    new commands: autosync, settoken, setgist

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-const { promisify } = require('util')
+const program = require('commander')
+const fetch = require('node-fetch')
+const {promisify} = require('util')
 const fs = require('fs')
+const moment = require('moment')
+
 const writeFile = promisify(fs.writeFile)
 const readFile = promisify(fs.readFile)
-const fetch = require('node-fetch')
+const fileExists = promisify(fs.exists)
+const stat = promisify(fs.stat)
+
 const fetchJson = (...args) => fetch(...args).then(r => r.json())
-const program = require('commander')
 
 const utf8 = 'utf8'
 
 const token = fs.readFileSync('token', utf8).trim()
-const authenticated = { headers: { Authorization: `token ${token}`}}
+const authenticated = {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}
 
 const baseUrl = 'https://api.github.com/'
 const gistId = fs.readFileSync('gist', utf8).trim()
@@ -29,14 +38,16 @@ const push = async fileName => {
     method: 'PATCH',
     body: JSON.stringify({
       files: {
-        [fileName]: { content }
+        [fileName]: {
+          content
+        }
       }
     })
   })
   console.log('done')
 }
 
-const ls = async () => {
+const ls = async() => {
   const gist = await fetchJson(specialGistUrl, authenticated)
   console.log(Object.keys(gist.files).join('\n'))
 }
@@ -54,32 +65,93 @@ const rm = async fileName => {
   console.log('done')
 }
 
-program
-  .version('0.1.0')
+const getLocalFile = async fileName => {
+  try {
+    const stats = await stat(fileName)
+    return {
+      mtime: stats.mtime,
+      content: await readFile(fileName, utf8)
+    }
+  } catch (e) {
+    return null
+  }
+}
 
-program
-  .command('pull [name]')
-  .description('Pull file from the gist')
-  .action(pull)
+const getRemoteFile = async fileName => {
+  const gist = await fetchJson(specialGistUrl, authenticated)
+  const rFile = gist.files[fileName]
+  if (!rFile) {
+    return null
+  } else {
+    let currentGist = gist
+    for (let h of gist.history) {
+      const oldGist = await fetchJson(h.url, authenticated)
+      const oldFile = oldGist.files[fileName]
+      const currentFile = currentGist.files[fileName]
+      if (!oldFile || currentFile.content !== oldFile.content) {
+        return {content: rFile.content, mtime: currentGist.updated_at}
+      }
+      currentGist = oldGist
+    }
+    return null
+  }
+}
+
+const sync = async fileName => {
+  const local = await getLocalFile(fileName)
   
-program
-  .command('push [name]')
-  .description('Push file to the gist')
-  .action(push)
+  if (!local) {
+    console.log('no local file, try to pull')
+    return pull(fileName)
+  }
+
+  const remote = await getRemoteFile(fileName)
   
-program
-  .command('rm [name]')
-  .description('Remove file from the gist')
-  .action(rm)
-  
-program
-  .command('ls')
-  .description('List all files in the gist')
-  .action(ls)
-  
+  if (!remote) {
+    console.log('no remote file, try to push')
+    return push(fileName)
+  }
+
+  if (moment(local.mtime).isAfter(moment(remote.mtime))) {
+    console.log('local file is newer, so push it')
+    return push(fileName)
+  } else {
+    console.log('remote file is newer, so pull it')
+    return pull(fileName)
+  }
+
+}
+
+const setToken = async token => {
+  await writeFile('token', token)
+  console.log('done');
+}
+
+const setGist = async gist => {
+  await writeFile('gist', gist)
+  console.log('done');
+}
+
+program.version('0.1.0')
+
+program.command('s [name]').description('Automatic sync').action(sync)
+
+program.command('pull [name]').description('Pull file from the gist').action(pull)
+
+program.command('push [name]').description('Push file to the gist').action(push)
+
+program.command('rm [name]').description('Remove file from the gist').action(rm)
+
+program.command('ls').description('List all files in the gist').action(ls)
+
+program.command('token [token]').description('Set a Personal access token -> see https://github.com/settings/tokens').action(setToken)
+
+program.command('gist [gist]').description('Set a gist id to use -> see https://gist.github.com/').action(setGist)
+
+
 program.parse(process.argv)
 
 if (!process.argv.slice(2).length) {
   program.outputHelp()
+  console.log('\n')
 }
-
